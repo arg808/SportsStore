2a039e036dcf85b4062a5e2c736b3095dd804382
Aaron Guidry <arg808@gmail.com>
2016-12-02 17:53:27 -0500

    Completed cart

--- src/SportsStore/Controllers/CartController.cs
+++ src/SportsStore/Controllers/CartController.cs
@@ -10,17 +10,19 @@ namespace SportsStore.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
 
         public ViewResult Index(string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
@@ -31,10 +33,8 @@ namespace SportsStore.Controllers
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
-            {
-                Cart cart = GetCart();
-                cart.AddItem(product, 1);
-                SaveCart(cart);
+            {                
+                cart.AddItem(product, 1);                
             }
             return RedirectToAction("Index", new { returnUrl });
         }
@@ -44,26 +44,12 @@ namespace SportsStore.Controllers
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
-            {
-                Cart cart = GetCart();
+            {        
                 cart.RemoveLine(product);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
-        }
-
-
-
+ 
     }
 }
